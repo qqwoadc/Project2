a4651b206c1a05905d2fafabe0ca29e5340d754a
zhujie <root@163.com>
2018-04-13 16:49:59 +0800

    修复了配置文件中被删除的类型转换器

--- MainModel/src/com/cskaoyan/bean/Consumption.java
+++ MainModel/src/com/cskaoyan/bean/Consumption.java
@@ -1,16 +1,105 @@
 package com.cskaoyan.bean;
 
+import java.io.StringReader;
 import java.util.Date;
 
 //消费类
 public class Consumption {
 
+    private int id;
+    private int srId;
     private String commodityName;
     private String commodityTypeName;
     private String commodityUOMName;
-    private String consumptionNumber;
-    private String commoditySalePrice;
-    private String consumptionMoney;
-    private Date consumptionTime;
+    private int consumptionNumber;
+    private float commoditySalePrice;
+    private float consumptionMoney;
+    private String consumptionTime;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSrId() {
+        return srId;
+    }
+
+    public void setSrId(int srId) {
+        this.srId = srId;
+    }
+
+    public String getCommodityName() {
+        return commodityName;
+    }
+
+    public void setCommodityName(String commodityName) {
+        this.commodityName = commodityName;
+    }
+
+    public String getCommodityTypeName() {
+        return commodityTypeName;
+    }
+
+    public void setCommodityTypeName(String commodityTypeName) {
+        this.commodityTypeName = commodityTypeName;
+    }
+
+    public String getCommodityUOMName() {
+        return commodityUOMName;
+    }
+
+    public void setCommodityUOMName(String commodityUOMName) {
+        this.commodityUOMName = commodityUOMName;
+    }
+
+    public int getConsumptionNumber() {
+        return consumptionNumber;
+    }
+
+    public void setConsumptionNumber(int consumptionNumber) {
+        this.consumptionNumber = consumptionNumber;
+    }
+
+    public float getCommoditySalePrice() {
+        return commoditySalePrice;
+    }
+
+    public void setCommoditySalePrice(float commoditySalePrice) {
+        this.commoditySalePrice = commoditySalePrice;
+    }
+
+    public float getConsumptionMoney() {
+        return consumptionMoney;
+    }
+
+    public void setConsumptionMoney(float consumptionMoney) {
+        this.consumptionMoney = consumptionMoney;
+    }
+
+    public String getConsumptionTime() {
+        return consumptionTime;
+    }
+
+    public void setConsumptionTime(String consumptionTime) {
+        this.consumptionTime = consumptionTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Consumption{" +
+                "id=" + id +
+                ", srId=" + srId +
+                ", commodityName='" + commodityName + '\'' +
+                ", commodityTypeName='" + commodityTypeName + '\'' +
+                ", commodityUOMName='" + commodityUOMName + '\'' +
+                ", consumptionNumber='" + consumptionNumber + '\'' +
+                ", commoditySalePrice='" + commoditySalePrice + '\'' +
+                ", consumptionMoney='" + consumptionMoney + '\'' +
+                ", consumptionTime=" + consumptionTime +
+                '}';
+    }
 }
--- MainModel/src/com/cskaoyan/bean/StayRegister.java
+++ MainModel/src/com/cskaoyan/bean/StayRegister.java
@@ -1,4 +1,83 @@
 package com.cskaoyan.bean;
 
+import java.util.Date;
+
 public class StayRegister {
+
+    private int srId;
+    private int predetermineId;
+    private int changingRoomNumber;
+    private float changRoomMoney;
+    private Date changRoomTime;
+    private Date registerTime;
+    private float sumConst;
+
+    @Override
+    public String toString() {
+        return "StayRegister{" +
+                "srId=" + srId +
+                ", predetermineId=" + predetermineId +
+                ", changingRoomNumber=" + changingRoomNumber +
+                ", changRoomMoney=" + changRoomMoney +
+                ", changRoomTime=" + changRoomTime +
+                ", registerTime=" + registerTime +
+                ", sumConst=" + sumConst +
+                '}';
+    }
+
+    public int getSrId() {
+        return srId;
+    }
+
+    public void setSrId(int srId) {
+        this.srId = srId;
+    }
+
+    public int getPredetermineId() {
+        return predetermineId;
+    }
+
+    public void setPredetermineId(int predetermineId) {
+        this.predetermineId = predetermineId;
+    }
+
+    public int getChangingRoomNumber() {
+        return changingRoomNumber;
+    }
+
+    public void setChangingRoomNumber(int changingRoomNumber) {
+        this.changingRoomNumber = changingRoomNumber;
+    }
+
+    public float getChangRoomMoney() {
+        return changRoomMoney;
+    }
+
+    public void setChangRoomMoney(float changRoomMoney) {
+        this.changRoomMoney = changRoomMoney;
+    }
+
+    public Date getChangRoomTime() {
+        return changRoomTime;
+    }
+
+    public void setChangRoomTime(Date changRoomTime) {
+        this.changRoomTime = changRoomTime;
+    }
+
+    public Date getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(Date registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public float getSumConst() {
+        return sumConst;
+    }
+
+    public void setSumConst(float sumConst) {
+        this.sumConst = sumConst;
+    }
 }
